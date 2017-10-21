2425d846c3237a814154c603e3aff2de364d0fa6
br1ghts1d3dev <vytychaknikolay@gmail.com>
2017-10-21 23:36:52 +0300

    app fix

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const Telegraf = require('telegraf');
 
 const BOT_TOKEN = '472750107:AAGxAMx0agxuxlgxUI_N7Hc-rp8qX6Nd6hY';
 
-const app = new Telegraf(BOT_TOKEN);
+const app = new Telegraf(BOT_TOKEN, {username: 'tg_memes_bot'});
 
 app.command('start', ({from, reply}) => {
     console.log('Started!');
