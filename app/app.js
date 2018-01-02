49105d432650374503edac9f1d91f4c15b71ce3f
br1ghts1d3dev <vytychaknikolay@gmail.com>
2018-01-02 13:50:44 +0200

    Markup test bot

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 const Telegraf = require('telegraf');
+const Markup = require('telegraf/markup');
 
 const BOT_TOKEN = '472750107:AAGxAMx0agxuxlgxUI_N7Hc-rp8qX6Nd6hY';
 
@@ -8,30 +9,30 @@ app.hears(/бот.*(с(а|у)мон.*пид(о|а)р|пид(о|а)р.*с(а|у)�
     ctx.reply('Эй, пидоры! @fansik @HappyKek @SevenFXD @br1ghts1d3 @XDShadow');
 });
 
-app.hears(/прив|hi|hello/i, (ctx) => {
+app.hears(/прив*|hi(\s|$)|hello(\s|$)/i, (ctx) => {
     const replies = ['Приветик', 'Здорова!']
     ctx.reply(replies[Math.floor(Math.random() * replies.length)]);
 });
 
-app.hears(/пидор|пидр|pidor|pidr|гей/i, (ctx) => {
+app.hears(/пидор(\s|$)|пидр(\s|$)|pidor(\s|$)|pidr(\s|$)|гей(\s|$)|homo(\s|$)/i, (ctx) => {
     const replies = ['Сам пидор! 🐔', 'Ты че, ахуел, пидор?! 🐔', 'Кто пидор? Я не пидор!', 'Че сразу пидор то?']
     ctx.reply(replies[Math.floor(Math.random() * replies.length)]);
 });
 
-app.hears(/blya|бля/i, (ctx) => {
+app.hears(/blya(\s|$)|бля(\s|$)/i, (ctx) => {
     const replies = ['БЛЯХО', 'Блэд!', 'ТЫ ЧЕ БЛЯ', 'БлЯдИнА ЙоБаНаЙа', 'УХХ БЛЭТ']
     ctx.reply(replies[Math.floor(Math.random() * replies.length)]);
 });
 
-app.hears(/Kappa/i, (ctx) => {
+app.hears(/Kappa(\s|$)/i, (ctx) => {
     ctx.replyWithPhoto('http://i0.kym-cdn.com/photos/images/facebook/000/925/494/218.png_large');
 });
 
-app.hears(/BibleThump/i, (ctx) => {
+app.hears(/BibleThump(\s|$)/i, (ctx) => {
     ctx.replyWithPhoto('https://ih1.redbubble.net/image.195693002.4115/flat,800x800,075,f.jpg');
 });
 
-app.hears(/\b(LUL|лул)/i, (ctx) => {
+app.hears(/\b(LUL(\s|$)|лул(\s|$))/i, (ctx) => {
     ctx.replyWithPhoto('https://ih0.redbubble.net/image.370068320.4729/flat,800x800,075,f.u2.jpg');
 });
 
@@ -54,12 +55,21 @@ app.hears(/поц/i, (ctx) => {
     ctx.replyWithPhoto(replies[Math.floor(Math.random() * replies.length)]);
 });
 
-app.hears(/ez|изи|рил|ток|real|talk|антихайп/i, (ctx) => {
+app.hears(/(ez|изи|рил|ток|real|talk|антихайп)(\s|$)/i, (ctx) => {
     ctx.replyWithPhoto('https://i.ytimg.com/vi/E-r4s7BvmiE/maxresdefault.jpg');
 });
 
-app.hears(/gachi|bass|гачи|басс/i, (ctx) => {
+app.hears(/(gachi|bass|гачи|басс)(\s|$)/i, (ctx) => {
     ctx.replyWithPhoto('https://yt3.ggpht.com/-hbETsOLQ9Zo/AAAAAAAAAAI/AAAAAAAAAAA/kRAuE3aXj4c/s900-c-k-no-mo-rj-c0xffffff/photo.jpg');
+});
+
+app.hears(/markup/i, ctx => {
+    ctx.reply('Text here: ', Markup
+        .keyboard(['hi', 'hello'])
+        .oneTime()
+        .resize()
+        .extra()
+    )
 });
 
 app.startPolling();
